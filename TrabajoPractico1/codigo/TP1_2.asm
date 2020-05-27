62c05a284980df6727ebe9ea60540fd48da8e9c1
Jose Maria Borgatello <65685919+JMBorgatello@users.noreply.github.com>
2020-05-27 18:41:45 -0300

    Subo nueva version TP 1b
    Puerto B abierto solo bit 0 como salida
    Corrijo indentado del codigo y detalles menores

--- TrabajoPractico1/codigo/TP1_2.asm
+++ TrabajoPractico1/codigo/TP1_2.asm
@@ -5,64 +5,81 @@
 ; Author : Jose Maria Borgatello
 ;
 
-.include "m328pdef.inc"
+	.include "m328pdef.inc"
 
-.equ PBX=0 ; Led en PB0
+; Led en PB0
+	.equ PBX=0
 
-.equ PUERTOB=0x01	;(PORTB completo como salida)
-
-.cseg 
-.org 0x0000
-			jmp		main
-
-.org INT_VECTORS_SIZE
-
+; (PORTB  con bit 0 solamente como salida)
+	.equ PUERTOB=0x01
+	
+	.cseg
+	
+	.org 0x0000
+	
+	jmp	main
+	
+	.org INT_VECTORS_SIZE
+	
 main:
-			
-; Configuro puerto B
-			ldi		r23,PUERTOB
-			out		DDRB,r23
 
+; Configuro puerto B
+
+	ldi	r23,PUERTOB
+	out	DDRB,r23
+	
+; rutina infinita
 xsiempre:
 
 ; rutina de encendido y apagado
-		
-prendo:		sbi		PORTB,PBX 	; encendido del led
+prendo:
+
+;encendido del led
+	sbi	PORTB,PBX
 	
-
+; Bucle Demora 1
 demora1:
-			ldi 	r20,0x00
-			ldi 	r21,0x00
-			ldi		r22,0x03
-ciclo1:		inc		r20
-			cpi		r20,0xff
-			brlo	ciclo1
-			ldi		r20,0x00
-			inc		r21
-			cpi		r21,0xff
-			brlo	ciclo1
-			ldi		r21,0x00
-			inc		r22
-			cpi		r22,0x20
-			brlo	ciclo1
-			
-			
-			cbi		PORTB,PBX		; apagado del led
+	ldi r20,0x00
+	ldi r21,0x00
+	ldi	r22,0x00
 
+ciclo1:
+	inc	r20
+	cpi	r20,0xff
+	brlo	ciclo1
+	ldi	r20,0x00
+	
+	inc	r21
+	cpi	r21,0xff
+	brlo	ciclo1
+	ldi	r21,0x00
+	
+	inc	r22
+	cpi	r22,0x20
+	brlo	ciclo1
+
+; apagado del led			
+	cbi	PORTB,PBX
+
+; Bucle Demora 2
 demora2:
-			ldi 	r20,0x00
-			ldi 	r21,0x00
-			ldi		r22,0x01
-ciclo2:		inc		r20
-			cpi		r20,0xff
-			brlo	ciclo2
-			ldi		r20,0x00
-			inc		r21
-			cpi		r21,0xff
-			brlo	ciclo2
-			ldi		r21,0x00
-			inc		r22
-			cpi		r22,0x20
-			brlo	ciclo2
+	ldi r20,0x00
+	ldi r21,0x00
+	ldi	r22,0x00
 
-			rjmp xsiempre
+ciclo2:
+	inc	r20
+	cpi	r20,0xff
+	brlo	ciclo2
+	ldi	r20,0x00
+	
+	inc	r21
+	cpi	r21,0xff
+	brlo	ciclo2
+	ldi	r21,0x00
+	
+	inc	r22
+	cpi	r22,0x20
+	brlo	ciclo2
+
+	rjmp xsiempre
